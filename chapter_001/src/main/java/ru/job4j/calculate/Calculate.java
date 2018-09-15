6ea30dd1071c8d6bc006d2b0b43ed61bd04b8ebb
IvanCheporov <mailto:vanessok@mail.ru>
2018-09-15 20:25:59 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j.calculate;
 
+/**
+*Class Calculate решение задачи из части chapter001.
+*@author Ivan Cheporov
+*@since 15.09.2018
+*@version 1.0
+*/
+ 
+
 public class Calculate{
+	/**
+	*Main.
+	*@param args-args.
+	*@return Hello World 
+	*/
 	public static void main (String []args){
 		System.out.println("Hello word");
 	}
