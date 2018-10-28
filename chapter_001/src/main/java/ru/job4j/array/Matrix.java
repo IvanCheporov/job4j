6fb3413ee2db37b8407620b696552384d48c5d3f
IvanCheporov <mailto:vanessok@mail.ru>
2018-10-28 18:25:45 +0300

    6.6 Двумерный массив. Таблица умножения

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,18 @@
+﻿package ru.job4j.array;
+
+/**
+ * @author Ivan Cheporov (vanessok@mail.ru)
+ * @version $1.0$
+ * @since 28.10.2018.
+ */
+public class Matrix {
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                    table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return table;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,24 @@
+﻿package ru.job4j.array;
+/**
+ * @author Ivan Cheporov (vanessok@mail.ru)
+ * @version $1.0$
+ * @since 28.10.2018.
+ */
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixTest {
+    @Test
+    public void when3on3() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(3);
+        int[][] expect = {
+                {1, 2, 3},
+                {2, 4, 6},
+                {3, 7, 9}
+        };
+        assertThat(table, is(expect));
+    }
+}
