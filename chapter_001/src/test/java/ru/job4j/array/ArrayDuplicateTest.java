c3b28eeb6989795a2408099ef8fdf3afcaf08f14
IvanCheporov <mailto:vanessok@mail.ru>
2018-11-02 19:41:13 +0300

    6.8 Удаление дубликатов в массиве

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,30 @@
+﻿package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Ivan Cheporov (vanessok@mail.ru)
+ * @version $1.0$
+ * @since 02.11.2018.
+ */
+
+public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        ArrayDuplicate duplicates = new ArrayDuplicate();
+        String[] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+        String[] result = duplicates.remove(input);
+        String[] expect = {"Привет", "Мир", "Супер"};
+        assertThat(result, arrayContainingInAnyOrder(expect));
+    }
+
+    public void whenArrayHasAllDuplicates() {
+        ArrayDuplicate duplicates = new ArrayDuplicate();
+        String[] input = {"Привет", "Привет", "Привет", "Привет", "Привет"};
+        String[] result = duplicates.remove(input);
+        String[] expect = {"Привет"};
+        assertThat(result, arrayContainingInAnyOrder(expect));
+    }
+}
