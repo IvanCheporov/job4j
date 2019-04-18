a9ed0a0440eef4c38d58f0e9ef5a7da06b287284
IvanCheporov <mailto:vanessok@mail.ru>
2019-04-18 21:45:39 +0300

    Рефакторинг - Перенести общие методы в абстрактный класс[#74647]_отдельное задание

--- chapter_002/src/main/java/ru/job4j/tracker/BaseAction.java
+++ chapter_002/src/main/java/ru/job4j/tracker/BaseAction.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 /**
  * @version 1.0
- * @since 04.04.2019
+ * @since 18.04.2019
  * @author Ivan Cheporov(vanessok@mail.ru)
  */
 public abstract class BaseAction implements UserAction {
