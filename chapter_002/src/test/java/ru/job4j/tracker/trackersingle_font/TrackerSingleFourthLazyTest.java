fb4c236a1a7d463564b182cb491ac2c3c20aabac
IvanCheporov <mailto:vanessok@mail.ru>
2019-04-24 22:38:09 +0300

    1. Singletone

---
+++ chapter_002/src/test/java/ru/job4j/tracker/trackersingle_font/TrackerSingleFourthLazyTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.trackersingle;
+
+import org.junit.Test;
+import ru.job4j.tracker.models.Item;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Ivan Cheporov (vanessok@mail.ru)
+ * @version 1.0
+ * @since 24.04.2019
+ */
+
+public class TrackerSingleFourthLazyTest {
+    @Test
+    public void whenCheckSingletonForPrivateStaticFinalClassLazy() {
+        TrackerSingleFourthLazy trackerFirst = TrackerSingleFourthLazy.getInstance();
+        TrackerSingleFourthLazy trackerSecond = TrackerSingleFourthLazy.getInstance();
+        assertThat(trackerFirst, is(trackerSecond));
+        Item item = new Item("test1", "testDescription", 123L);
+        Item item2 = new Item("test2", "testDescription2", 1234L);
+        trackerFirst.add(item);
+        trackerSecond.add(item2);
+        assertThat(trackerFirst.getAll().get(0), is(trackerSecond.getAll().get(0)));
+        assertThat(trackerFirst.getAll().get(1), is(item2));
+    }
+}
